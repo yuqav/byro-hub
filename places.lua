a04ad0bb4f58a9bbefc58063ce2837fe872cf04b
yuqav <tronyax@gmail.com>
2024-12-11 21:43:05 +0300

    Update places.lua

--- places.lua
+++ places.lua
@@ -1,3 +1,7 @@
 if game.PlaceId == 116605585218149 then --Go Fishing
      loadstring(game:HttpGet("https://raw.githubusercontent.com/yuqav/byro-hub/refs/heads/main/go%20fishing.lua"))()
 end
+
+if game.PlaceId == 16953799253 then -- Player Rng
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/yuqav/byro-hub/refs/heads/main/player%20rng.lua"))()
+end
